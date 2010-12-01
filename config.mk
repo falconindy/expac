74595bdf80235a269c3e39a0ed6e1bb9e49937cd
Dave Reisner <d@falconindy.com>
2010-11-30 20:01:26 -0500

    add manpage to install target

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 # version
 VERSION = $(shell git describe --always)
 
+# paths
+PREFIX ?= /usr/local
+MANPREFIX ?= ${PREFIX}/share/man
+
 # compiler flags
 CC = c99
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${PMCHECK} 
